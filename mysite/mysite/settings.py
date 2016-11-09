28b8e8b3174b05798cae75bfff9917fb8e4a091a
roshind <roshindvp@gmail.com>
2016-11-09 16:18:20 +0530

    settings_edit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_-sb%2bdz*tva^ss2&c1(ilh7+mf81n--#ge=!_0_yxs&o(n2d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
